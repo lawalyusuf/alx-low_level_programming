81094936ad9d34075b296dad5f75216b4f2ec66d
Lawal Owolabi Y <bwitlawalyusuf@gmail.com>
2022-06-07 20:27:08 -0700

    prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * main - Entry point
@@ -8,7 +8,7 @@ include "main.h"
 
 int main(void)
 {
-	_putchar('');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
