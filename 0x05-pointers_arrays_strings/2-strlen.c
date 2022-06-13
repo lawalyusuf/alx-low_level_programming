f10989d624cf4326b217aa041f3f8f4e684a03d3
Lawal Owolabi Y <bwitlawalyusuf@gmail.com>
2022-06-13 16:38:57 -0700

    returns the length of a string.

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,12 +3,14 @@
 /**
  * _strlen - Returns the length of a string.
  * @str: string.
- * Return: Length.
+ *
+ *Return: Length.
  */
 
 size_t _strlen(const char *str)
 {
 	size_t len = 0;
+
 	while (*str++)
 		len++;
 
