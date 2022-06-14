63eb18fdf75802325424744dfe0de0fdea850558
Lawal Owolabi Y <bwitlawalyusuf@gmail.com>
2022-06-13 17:43:50 -0700

    generate random valid password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,10 +1,13 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - generates keygen.
  * Return: 0 Always.
  */
+
 int main(void)
 {
 	int r = 0, c = 0;
