5bfa7fd4c3700a599fdbcedcea4805e1c2117af6
Lawal Owolabi Y <bwitlawalyusuf@gmail.com>
2022-06-13 18:14:43 -0700

    code update to generate random key

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,41 +1,50 @@
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <math.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h> 
+
 /**
  * main - generates keygen.
  * Return: 0 Always.
  */
 
-int main(void)
+void randomPasswordGeneration(int N)
 {
-    char a[10],c[10],d;
-    int i,j,k=0;
-    printf("#Keygen by b44nz0r\n\n");
-    
-    while (k <5 || k >=10)
-    {
-          if (k !=0)
-             printf("\nThe username length should be 5 to 10 alphabets\n");
-        
-         printf("enter username: ");
-         scanf("%s",a);
-         k = strlen(a);
-    }
-    
-    i = k-1;
-    j = 0;
-    
-    while (i >= 0)
-    {
-          c[j] = a[i]+i;
-          i--;
-          j++;
-    }
-    
-    c[j] = 0;
-    printf("\nThe password is %s\n",c);
-    printf("\nHit Enter to Exit\n");
-    getchar();
-    getchar();
-} 
+	int i = 0;
+
+	int randomizer = 0;
+
+	srand((unsigned int)(time(NULL)));
+	char numbers[] = "0123456789";
+	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+	char symbols[] = "!@#$^&*?";
+	char password[N];
+	randomizer = rand() % 4;
+
+	for (i = 0; i < N; i++){
+		if (randomizer == 1) {
+			password[i] = numbers[rand() % 10];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 2) {
+			password[i] = symbols[rand() % 8];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else {
+			password[i] = letter[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+	}
+}
+
+int main()
+{
+	int N = 10;
+
+	randomPasswordGeneration(N);
+
+	return 0;
+}
